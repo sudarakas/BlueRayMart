26f5860ee46c49fb1cc7065398fd2bea981d8ea3
sudarakas <sudharakafb@gmail.com>
2019-07-03 00:03:53 +0530

    BillingAddress and ShippingAddress class models added

---
+++ src/main/java/com/blueraymart/model/BillingAddress.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.blueraymart.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+/**
+ *
+ * @author NanoX
+ */
+@Entity
+public class BillingAddress implements Serializable{
+
+    private static final long serialVersionUID = -8362596108658102714L;
+    
+    @Id
+    @GeneratedValue
+    private int billingAddressId;
+    private String streetName;
+    private String apartmentNumber;
+    private String city;
+    private String state;
+    private String country;
+    private String postalCode;
+    
+    @OneToOne
+    private Customer customer;
+
+    public int getBillingAddressId() {
+        return billingAddressId;
+    }
+
+    public void setBillingAddressId(int billingAddressId) {
+        this.billingAddressId = billingAddressId;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getApartmentNumber() {
+        return apartmentNumber;
+    }
+
+    public void setApartmentNumber(String apartmentNumber) {
+        this.apartmentNumber = apartmentNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+    
+    
+    
+}
---
+++ src/main/java/com/blueraymart/model/ShippingAddress.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.blueraymart.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+/**
+ *
+ * @author NanoX
+ */
+@Entity
+public class ShippingAddress implements Serializable{
+
+    private static final long serialVersionUID = 1670129604970224406L;
+    
+    @Id
+    @GeneratedValue
+    private int shippingAddressId;
+    private String streetName;
+    private String apartmentNumber;
+    private String city;
+    private String state;
+    private String country;
+    private String postalCode;
+    
+    @OneToOne
+    private Customer customer;
+
+    public int getShippingAddressId() {
+        return shippingAddressId;
+    }
+
+    public void setShippingAddressId(int shippingAddressId) {
+        this.shippingAddressId = shippingAddressId;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getApartmentNumber() {
+        return apartmentNumber;
+    }
+
+    public void setApartmentNumber(String apartmentNumber) {
+        this.apartmentNumber = apartmentNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    @Override
+    public String toString() {
+        return "ShippingAddress{" + "streetName=" + streetName + ", apartmentNumber=" + apartmentNumber + ", city=" + city + ", state=" + state + ", country=" + country + ", postalCode=" + postalCode + '}';
+    }
+    
+    
+    
+}
